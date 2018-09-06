2b73d766619465e15d2891da1d0b189ac6842b7b
Jarvis Schultz <schultzjarvis@gmail.com>
2018-09-06 16:17:29 -0500

    Minor changes to setup.py for PyPi release

--- packages/Python/setup.py
+++ packages/Python/setup.py
@@ -2,6 +2,18 @@ from setuptools import setup
 
 exec(open('modern_robotics/__version__.py').read())
 
+long_description = """
+# Modern Robotics:  Mechanics, Planning, and Control Code Library
+
+This package contains the Python code accompanying [_Modern Robotics:
+Mechanics, Planning, and Control_](http://modernrobotics.org) (Kevin Lynch
+and Frank Park, Cambridge University Press 2017).
+
+The primary purpose of the provided software is to be easy to read and educational, reinforcing the concepts in the book. The code is optimized neither for efficiency nor robustness.
+
+For more information, including a user manual, see the [project's GitHub page](https://github.com/NxRLab/ModernRobotics).
+"""
+
 setup(
     name = "modern_robotics",
     version = __version__,
@@ -9,6 +21,8 @@ setup(
     author_email = "huanweng@u.northwestern.edu",
     description = ("Modern Robotics: Mechanics, Planning, and Control: Code Library"),
     license = "MIT",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     keywords = "kinematics robotics dynamics",
     url = "http://modernrobotics.org/",
     packages=['modern_robotics'],
