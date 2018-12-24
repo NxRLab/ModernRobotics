21586238aafa0f3468115ffe6a6d6fd159a29763
HuanWeng <huanweng2015@u.northwestern.edu>
2018-12-24 02:05:24 -0600

    Debug MatrixLog3 and MatrixLog6 for MATLAB and Mathematica

--- packages/Matlab/mr/MatrixLog3.m
+++ packages/Matlab/mr/MatrixLog3.m
@@ -15,25 +15,23 @@ function so3mat = MatrixLog3(R)
 %    1.2092         0   -1.2092
 %   -1.2092    1.2092         0
 
-if NearZero(norm(R - eye(3)))
-	so3mat = zeros(3);
-elseif NearZero(trace(R) + 1)
+acosinput = (trace(R) - 1) / 2;
+if acosinput >= 1
+    so3mat = zeros(3);
+elseif acosinput <= -1
     if ~NearZero(1 + R(3, 3))
-        omg = (1 / sqrt(2 * (1 + R(3, 3)))) * [R(1, 3); R(2, 3); 1 + R(3, 3)];
+        omg = (1 / sqrt(2 * (1 + R(3, 3)))) ...
+              * [R(1, 3); R(2, 3); 1 + R(3, 3)];
     elseif ~NearZero(1 + R(2, 2))
-        omg = (1 / sqrt(2 * (1 + R(2, 2)))) * [R(1, 2); 1 + R(2, 2); R(3, 2)];
+        omg = (1 / sqrt(2 * (1 + R(2, 2)))) ...
+              * [R(1, 2); 1 + R(2, 2); R(3, 2)];
     else
-        omg = (1 / sqrt(2 * (1 + R(1, 1)))) * [1 + R(1, 1); R(2, 1); R(3, 1)];
+        omg = (1 / sqrt(2 * (1 + R(1, 1)))) ...
+              * [1 + R(1, 1); R(2, 1); R(3, 1)];
     end
     so3mat = VecToso3(pi * omg);
 else
-    acosinput = (trace(R) - 1) / 2;
-    if acosinput > 1
-        acosinput = 1;
-    elseif acosinput < -1
-        acosinput = -1;
-    end
-    theta = acos(acosinput);
+	theta = acos(acosinput);
     so3mat = theta * (1 / (2 * sin(theta))) * (R - R');
 end
 end
